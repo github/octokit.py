d15411bbb215cc96bd61b90203dfb77cdedebac7
Ben Toews <mastahyeti@gmail.com>
2013-09-05 12:46:22 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Octokit.py
+
+A GitHub API client for Python.
+
+## Testing
+
+- Copy `test_config.sh` to `.test_config.sh`
+- Configure `.test_config.sh` with username and OAuth token.
+- Run `./tests.sh`
--- test.py
+++ test.py
@@ -7,13 +7,13 @@ import octokit
 
 class OctokitTestCase(unittest.TestCase):
   def setUp(self):
-    username = os.environ['OCTOKIT_TEST_USER']
-    token = os.environ['OCTOKIT_TEST_TOKEN']
-    auth = (username, token)
+    self.username = os.environ['OCTOKIT_TEST_USER']
+    self.token = os.environ['OCTOKIT_TEST_TOKEN']
+    auth = (self.username, self.token)
     self.client = octokit.Client(auth=auth)
 
   def test_authentication(self):
-    assert self.client.current_user.login == 'mastahyeti'
+    assert self.client.current_user.login == self.username
 
 if __name__ == '__main__':
     unittest.main()
