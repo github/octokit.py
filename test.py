63bfb6a668e5f69b50050dbc984bc3ff7f92244f
Ho-Wei Kang <howeik@gmail.com>
2015-10-09 13:31:18 -0700

    travis.yml and script/test

--- test.py
+++ test.py
@@ -7,13 +7,17 @@ import octokit
 
 class OctokitTestCase(unittest.TestCase):
   def setUp(self):
-    self.username = os.environ['OCTOKIT_TEST_USER']
-    self.token = os.environ['OCTOKIT_TEST_TOKEN']
+    self.username = os.environ['OCTOKIT_TEST_GITHUB_LOGIN']
+    self.token = os.environ['OCTOKIT_TEST_GITHUB_TOKEN']
     auth = (self.username, self.token)
     self.client = octokit.Client(auth=auth)
 
+  # TODO: make this test pass
   def test_authentication(self):
     assert self.client.current_user.login == self.username
+
+  def test_true(self):
+    assert True
 
 if __name__ == '__main__':
   unittest.main()
--- test_config.sh
+++
@@ -1,4 +0,0 @@
-# Move this to .test_config.sh once configured avoid it ending up in the repo
-
-export OCTOKIT_TEST_USER="username"
-export OCTOKIT_TEST_TOKEN="oauth token"
