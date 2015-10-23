e1d66a05109fb880d4c554ae06bcdead662b224f
Ho-Wei Kang <howeik@gmail.com>
2015-10-22 23:27:46 -0700

    Changed hardcoded login name to self.login

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,7 +7,7 @@ class TestApi(MockHttpTestCase):
     def test_current_user(self):
         with self.recorder.use_cassette('current_user'):
             login = self.client.current_user.login
-            assert login == 'api-padawan'
+            assert login == self.login
 
 if __name__ == '__main__':
     unittest.main()
