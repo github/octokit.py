60d5c3909f6e664909b3a383a79120c162a74a25
Ho-Wei Kang <howeik@gmail.com>
2015-10-22 22:30:12 -0700

    Refactored and PEP8'd test_httpverb

---
+++ tests/test_httpverb.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+
+import octokit
+
+import requests_mock
+import uritemplate
+
+class TestHttpVerb(unittest.TestCase):
+    """Basic test to ensure that the octokit client can use multiple HTTP verbs
+    and handle a simple JSON response.
+    """
+
+    def setUp(self):
+        self.client = octokit.Client(api_endpoint='mock://api.com/{param}')
+        self.adapter = requests_mock.Adapter()
+        self.client.session.mount('mock', self.adapter)
+
+    def test_httpverb(self):
+        verbs_to_methods = [
+            ('GET', self.client.get),
+            ('POST', self.client.post),
+            ('PUT', self.client.put),
+            ('PATCH', self.client.patch),
+            ('DELETE', self.client.delete),
+            ('HEAD', self.client.head),
+            ('OPTIONS', self.client.options),
+        ]
+
+        for verb, method in verbs_to_methods:
+            url = uritemplate.expand(self.client.url, {'param': 'foo'})
+            self.adapter.register_uri(verb, url, text='{"success": true}')
+
+            response = method(param='foo')
+            assert response.success
+
+            # test the named param inference
+            response = method('foo')
+            assert response.success
+
+if __name__ == '__main__':
+    unittest.main()
