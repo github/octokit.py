2ef6d78878c1a31b4c1e71ed84cd1d4ba4db0737
Ho-Wei Kang <howeik@gmail.com>
2015-10-23 09:24:13 -0700

    Set TRAVIS flag in travis.yml

--- tests/util.py
+++ tests/util.py
@@ -21,7 +21,7 @@ class MockOctokitTestCase(unittest.TestCase):
         with betamax.Betamax.configure() as config:
             config.cassette_library_dir = 'tests/cassettes'
 
-            record_mode = 'never' if os.environ.get('TRAVIS_GH3') else 'once'
+            record_mode = 'never' if os.environ.get('TRAVIS') else 'once'
             config.default_cassette_options['record_mode'] = record_mode
 
             config.define_cassette_placeholder(
