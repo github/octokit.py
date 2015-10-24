5480c46dbf47bd7196e6417d250cae668615c86e
Ho-Wei Kang <howeik@gmail.com>
2015-10-23 20:37:30 -0700

    Renamed test_httpverb to test_resources; __call__ test

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -6,17 +6,28 @@ import uritemplate
 
 import octokit
 
-class TestHttpVerb(unittest.TestCase):
-    """Basic test to ensure that the octokit client can use multiple HTTP verbs
-    and handle a simple JSON response.
-    """
+class TestResources(unittest.TestCase):
+    """Tests the functionality in octokit/resources.py"""
 
     def setUp(self):
         self.client = octokit.Client(api_endpoint='mock://api.com/{param}')
         self.adapter = requests_mock.Adapter()
         self.client.session.mount('mock', self.adapter)
 
+    def test_call(self):
+        """Test that resources.__call__ performs a HTTP GET"""
+        url = uritemplate.expand(self.client.url, {'param': 'foo'})
+        self.adapter.register_uri('GET', url, text='{"success": true}')
+
+        response = self.client(param='foo')
+        assert response.success
+
+        # test named param inference
+        response = self.client('foo')
+        assert response.success
+
     def test_httpverb(self):
+        """Test that each HTTP verb functions properly when JSON is returned."""
         verbs_to_methods = [
             ('GET', self.client.get),
             ('POST', self.client.post),
